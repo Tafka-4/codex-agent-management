27aba99276db4f749a7c33ad69e8692e8404ce45
Tafka-4 <shkh071216@gmail.com>
2025-11-19 13:21:12 +0900

    Chore: remove payload limit

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { registerWebsocketServer } from "./websocket.js";
 const app = express();
 
 app.disable("x-powered-by");
-app.use(express.json());
+app.use(express.json({ limit: "256mb" }));
 
 app.use("/api", indexRouter);
 
@@ -14,14 +14,21 @@ app.use((req, res) => {
 	res.status(404).json({ error: "Route not found." });
 });
 
-app.use((error: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-	if (res.headersSent) {
-		return next(error);
-	}
+app.use(
+	(
+		error: Error,
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		if (res.headersSent) {
+			return next(error);
+		}
 
-	console.error(error);
-	res.status(500).json({ error: "Internal server error." });
-});
+		console.error(error);
+		res.status(500).json({ error: "Internal server error." });
+	}
+);
 
 const port = Number(process.env.PORT ?? 3000);
 const server = createServer(app);
